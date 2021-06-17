50be2f0a4b7d321c9a699e39c88cd1dd0bfc99dc
Christian Nuss <cnuss@users.noreply.github.com>
2021-06-17 14:02:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,9 +36,6 @@ module "bootstrap" {
     cdn = {
       template = "scaffoldly/web-cdn-template"
     }
-    cdn2 = {
-      template = "scaffoldly/web-cdn-template"
-    }
   }
   
   serverless_apis = {

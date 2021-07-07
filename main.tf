288e1df25ee0c70d697a1d6c1aa7c07e2d500dc8
Christian Nuss <cnuss@users.noreply.github.com>
2021-07-07 16:15:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,5 +42,8 @@ module "bootstrap" {
     example = {
       template = "scaffoldly/sls-rest-api-template"
     }
+    jimmie = {
+      template = "scaffoldly/sls-rest-api-template"
+    }
   }
 }
